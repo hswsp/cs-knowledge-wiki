7631f78bb979fea9a40077595a1c7794fec4569c
hswsp <hswsp@mail.ustc.edu.cn>
2026-06-13 17:11:52 +0800

    fix index

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -62,14 +62,6 @@ export default defineConfig({
       { text: '单纯形算法求解线性规划', link: '/math/单纯形算法求解线性规划' },
     ],
   },
-  {
-    text: 'Information Theory',
-    collapsed: true,
-    items: [
-      { text: '有限域算术: 从 AES 到 Reed-Solomon', link: '/math/有限域算术-从-aes-到-reed-solomon' },
-      { text: 'RS码原理及柯西优化', link: '/math/rs码原理及柯西优化' },
-    ],
-  },
 ],
           '/algorithm/': [
   { text: '算法', items: [
